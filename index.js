057430b28d7e9fcf24c87db048f925e53572a67d
Fabio Gatto <fabio.gatto@soprasteria.com>
2023-04-14 00:52:24 +0200

    first version

---
+++ index.js
@@ -0,0 +1,85 @@
+#!/usr/bin/env node
+//✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤
+const support = [
+    { prefix: "data:image/jpeg;base64," , mimes: ["image/jpg","image/jpeg","image/x-citrix-jpeg","image/pjpeg"] , extensions: [".jpg",".jpeg"] },
+    { prefix: "data:image/png;base64,"  , mimes: ["image/png","image/x-png","image/x-citrix-png"]               , extensions: [".png"]         },
+    { prefix: "data:image/gif;base64,"  , mimes: ["image/gif"]                                                  , extensions: [".gif"]         },
+    { prefix: "data:image/bmp;base64,"  , mimes: ["image/bmp"]                                                  , extensions: [".bmp"]         },
+    { prefix: "data:image/tiff;base64," , mimes: ["image/tiff","image/tif"]                                     , extensions: [".tif",".tiff"] },
+];
+//✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤
+const args = process.argv.slice(2);
+if (args.length == 0) {
+    const pkg = require("./package.json");
+    const supportFormats = function() {
+        var resp = [];
+        for (var i=0;i<support.length;i++) resp.push(...support[i].extensions);
+        return resp.sort().join(", ");
+    };
+    const supportMimes = function() {
+        var resp = [];
+        for (var i=0;i<support.length;i++) resp.push(...support[i].mimes);
+        return resp.sort().join(", ");
+    };
+    console.log(
+`
+    software          : ${pkg.name} (v${pkg.version})
+    information       : ${pkg.description}
+    author            : ${pkg.author}
+    usage             : ${pkg.name} <url|file>
+    supported formats : ${supportFormats()}
+    supported mimes   : ${supportMimes()}
+`
+    );
+    process.exit(0);
+}
+//✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤
+const isUrl = require("is-url");
+const request = require("sync-request");
+const fs = require("fs");
+const PATH = require("path");
+const prefixFromContentType = function(contentType) {
+    if (typeof contentType != "string") return null;
+    contentType = contentType.toLowerCase();
+    for (var i=0;i<support.length;i++) {
+        if (support[i].mimes.indexOf(contentType)>=0) {
+            return support[i].prefix;
+        }
+    }
+    return null;
+};
+const prefixFromPath = function(path) {
+    if (typeof path != "string") return null;
+    var extension = PATH.extname(path).toLowerCase();
+    for (var i=0;i<support.length;i++) {
+        if (support[i].extensions.indexOf(extension)>=0) {
+            return support[i].prefix;
+        }
+    }
+    return null;
+};
+//✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤
+while (args.length > 0) {
+    var arg = args.shift();
+    try {
+        if (isUrl(arg)) {
+            var data = request("GET",arg);
+            var dtyp = data.headers["content-type"];
+            var pref = prefixFromContentType(dtyp);
+            if (pref == null) throw new Error("mime format not admitted : " + dtyp);
+            var base = data.getBody("base64");
+            console.log(pref+base);
+        } else {
+            var pref = prefixFromPath(arg);
+            if (pref == null) throw new Error("file format not admitted : " + arg);
+            var data = fs.readFileSync(arg,"binary"); 
+            var base = Buffer.from(data,"binary").toString("base64");
+            console.log(pref+base);
+        }
+    } catch(e) {
+        console.error(arg,e);
+    }
+}
+//✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤✤
+
+
